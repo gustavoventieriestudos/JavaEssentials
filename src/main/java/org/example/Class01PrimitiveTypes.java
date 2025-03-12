56a96fd0e11cce2480528e381f54282c3c9576a0
gustavoventieri <ventierigustavo@gmail.com>
2025-03-11 23:50:27 -0300

    feat: class 01 added

---
+++ src/main/java/org/example/Class01PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Class01PrimitiveTypes {
+
+    // int, double, float, boolean, char, byte, short, long
+
+
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println("The age is " + age);
+    }
+
+}
