13801c689568180aeb6ad882fc967d17b91c95f6
gustavoventieri <ventierigustavo@gmail.com>
2025-03-14 04:18:05 -0300

    feat: looṕ lessons added

---
+++ src/main/java/org/example/Class07WHILE.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Class07WHILE {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while(counter <= 10){
+
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
---
+++ src/main/java/org/example/Class08DO_WHILE.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Class08DO_WHILE {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        do {
+            System.out.println(counter);
+            counter++;
+        }while (counter <= 10);
+    }
+}
