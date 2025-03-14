e5dae6a82e1a6fc486e5c592134c0cc529169d53
gustavoventieri <ventierigustavo@gmail.com>
2025-03-14 05:06:30 -0300

    feat: class 10 - for break added

---
+++ src/main/java/org/example/Class10FOR_BREAK.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Class10FOR_BREAK {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i >= 25){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
