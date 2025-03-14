6bb07980645cbc599c6cfe7b5149bbf4b9a9d3ea
gustavoventieri <ventierigustavo@gmail.com>
2025-03-14 05:06:58 -0300

    feat: class 11/12 - arrays added and for each

---
+++ src/main/java/org/example/Class11Arrays.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Class11Arrays {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = i;
+        }
+        System.out.println(Arrays.toString(ages));
+    }
+
+
+}
---
+++ src/main/java/org/example/Class12Arrays_FOREACH.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Class12Arrays_FOREACH {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = i+2;
+        }
+        for (int age: ages){
+            System.out.println(age);
+        }
+    }
+}
