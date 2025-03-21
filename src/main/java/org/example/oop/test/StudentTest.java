6b2e893699b8c78ac87bfb586894a1125e487ff0
gustavoventieri <ventierigustavo@gmail.com>
2025-03-21 05:14:36 -0300

    feat - class 15 - two objects was initialized and used

--- src/main/java/org/example/oop/test/StudentTest.java
+++ src/main/java/org/example/oop/test/StudentTest.java
@@ -4,12 +4,17 @@ import org.example.oop.domain.Student;
 
 public class StudentTest {
    public static void main(String[] args) {
-    Student student = new Student();
+   Student student = new Student();
+   Student student2 = new Student();
 
     student.gender = "M";
     student.age = 10;
     student.name = "ion";
 
-    System.out.println(student.age);
+    student2.gender = "F";
+    student2.age = 12;
+    student2.name = "Ls";
+
+    System.out.println(student2.age);
    }
 }
