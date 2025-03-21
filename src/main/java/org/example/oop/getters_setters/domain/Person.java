d2e91dfd9baaa241c545449592fe4b64bf7d264f
gustavoventieri <ventierigustavo@gmail.com>
2025-03-21 05:40:07 -0300

    feat: class 18 - getters and setters added

---
+++ src/main/java/org/example/oop/getters_setters/domain/Person.java
@@ -0,0 +1,21 @@
+package org.example.oop.getters_setters.domain;
+
+public class Person {
+    public String name;
+    private int age;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+}
---
+++ src/main/java/org/example/oop/getters_setters/test/PersonTest.java
@@ -0,0 +1,14 @@
+package org.example.oop.getters_setters.test;
+
+import org.example.oop.getters_setters.domain.Person;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setAge(10);
+        person.setName("Gustavo");
+
+        
+    }
+}
